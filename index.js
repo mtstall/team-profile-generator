d522a70e7036016890ba0af5ce5cc50f57db69a6
mtstall <mtstallings95@gmail.com>
2023-02-05 19:25:33 -0500

    adding js code

--- index.js
+++ index.js
@@ -2,80 +2,246 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Manager = require("./lib/Manager.js");
-console.log(Manager);
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const html = require("./dist/generatehtml.js");
 
 // array of questions for user input
 const managerQuestions = [
-    {
-        type: "input",
-        message: "Enter manager name:",
-        name: "managername",
-        default: "Mr Manager",
-        validate: validateInput,
-      },
-      {
-        type: "input",
-        message: "Enter manager employee ID:",
-        name: "managerempid",
-        default: "1234",
-        validate: validateInput,
-      },
-      {
-        type: "input",
-        message: "Enter manager email:",
-        name: "manageremail",
-        default: "manager@business.com",
-        validate: validateInput,
-      },
-      {
-        type: "input",
-        message: "Enter manager office number:",
-        name: "managerofficenum",
-        default: "123",
-        validate: validateInput,
-      },
-      {
-        type: "list",
-        choices: [
-            {name: 'Engineer'},
-            {name: 'Intern'},
-            {name: 'Finish building your team.'},
-        ],
-        message: "Add an engineer, add an intern, or finish building your team.",
-        name: "nextoption",
-        default: "Add an engineer to the team",
-      }
-]
+  {
+    type: "input",
+    message: "Enter manager name:",
+    name: "name",
+    default: "Mr Manager",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter manager employee ID:",
+    name: "empid",
+    default: "1234",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter manager email:",
+    name: "email",
+    default: "manager@business.com",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter manager office number:",
+    name: "xtrainfo",
+    default: "123",
+    validate: validateInput,
+  },
+  {
+    type: "list",
+    choices: [
+      { name: "Engineer" },
+      { name: "Intern" },
+      { name: "Finish building your team." },
+    ],
+    message: "Add an engineer, add an intern, or finish building your team.",
+    name: "nextoption",
+    default: "Add an engineer to the team",
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "Enter employee name:",
+    name: "name",
+    default: "Mr Engineer",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter engineer employee ID:",
+    name: "empid",
+    default: "1234",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter engineer email:",
+    name: "email",
+    default: "engineer@business.com",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter engineer Github:",
+    name: "xtrainfo",
+    default: "engineer_01",
+    validate: validateInput,
+  },
+  {
+    type: "list",
+    choices: [
+      { name: "Engineer" },
+      { name: "Intern" },
+      { name: "Finish building your team." },
+    ],
+    message: "Add an engineer, add an intern, or finish building your team.",
+    name: "nextoption",
+    default: "Add an engineer to the team",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    message: "Enter employee name:",
+    name: "name",
+    default: "Mr Intern",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter intern employee ID:",
+    name: "empid",
+    default: "1234",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter intern email:",
+    name: "email",
+    default: "intern@business.com",
+    validate: validateInput,
+  },
+  {
+    type: "input",
+    message: "Enter intern's school:",
+    name: "xtrainfo",
+    default: "University of Interns",
+    validate: validateInput,
+  },
+  {
+    type: "list",
+    choices: [
+      { name: "Engineer" },
+      { name: "Intern" },
+      { name: "Finish building your team." },
+    ],
+    message: "Add an engineer, add an intern, or finish building your team.",
+    name: "nextoption",
+    default: "Add an engineer to the team",
+  },
+];
+
+let teamArray = [];
 
 // initialize function
 function init(response) {
-    inquirer.prompt(managerQuestions).then((response) => {
-      const manager = new Manager(response.name,response.id,response.email,response.managerofficenum);
-        if (response.nextoption === 'Finish building your team.') {
-        generateManagerHTML(manager);
-        }
-    return; 
-    }
-    )
-  }
-  
-  init();
+  inquirer.prompt(managerQuestions).then((response) => {
+    const manager = new Manager(
+      response.name,
+      response.empid,
+      response.email,
+      response.xtrainfo
+    );
+    // add manager to team array
+    teamArray.push(manager);
+    nextQuestions(response);
+  });
+}
 
-  // call this function when user is done building out team
-  function generateManagerHTML (manager) {
-    fs.writeFile("./dist/index.html",
+init();
+
+// code to decide which set of questions to run next
+function nextQuestions(response) {
+  if (response.nextoption === "Finish building your team.") {
+    // generate html
+    generateHTML(teamArray);
+  }
+  if (response.nextoption === "Engineer") {
+    // run engineer inquirer
+    engineerInquirer();
+  }
+  else if (response.nextoption === "Intern") {
+    // run intern inquirer
+    internInquirer();
+  }
+}
+
+function generateHTML (teamArray) {
+  console.log(teamArray);
+  if (teamArray.getRole() === 'Manager') {
+    let xtrainfo = 
+  }
+  console.log(teamArray[0].getRole());
+  console.log(teamArray[0].xtrainfo);
+  for (let i = 0; i < teamArray.length; i++) {
+    //generate a card for each team member
+    return `
+    <div class="card">
+<div class="card-header">
+    <h2>${teamArray[i].name}</h2>  
+    <h2><i class="far fa-user"></i> ${teamArray[i].getRole()}</h2>
+    <hr>
+</div>
+<div class="card-body">
+    <ul>
+        <li>ID: ${teamArray[i].empid}</li>
+        <li>Email: ${teamArray[i].email}</li>
+        <li>${teamArray[i].xtrainfo} </li>
+    </ul>
+</div>
+</div>
+    `
+  }
+
+}
+
+function engineerInquirer(response) {
+  inquirer.prompt(engineerQuestions).then((response) => {
+    const engineer = new Engineer(
+      response.name,
+      response.empid,
+      response.email,
+      response.xtrainfo,
+    );
+    // add new engineer team member to team array
+    teamArray.push(engineer);
+    nextQuestions(response);
+  });
+}
+
+function internInquirer(response) {
+  inquirer.prompt(internQuestions).then((response) => {
+    const intern = new Intern(
+      response.name,
+      response.empid,
+      response.email,
+      response.xtrainfo,
+    );
+    // add new intern team member to team array
+    teamArray.push(intern);
+    nextQuestions(response);
+  });
+}
+
+// call this function when user is done building out team
+function generateManagerHTML(manager) {
+  fs.writeFile(
+    "./dist/index.html",
     `manager role: 
     ${manager.getRole()}
-    `, (err) => err ? console.log(err) : console.log("HTML generated!"));
-  }
-
+    `,
+    (err) => (err ? console.log(err) : console.log("HTML generated!"))
+  );
+}
 
 // validate input
 function validateInput(input) {
-    if (input === "") {
-      console.log("Please enter a value.");
-      return false;
-    } else {
-      return true;
-    }
+  if (input === "") {
+    console.log("Please enter a value.");
+    return false;
+  } else {
+    return true;
   }
+}
