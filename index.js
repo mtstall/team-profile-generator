2ee046b35fcce362bd798dadec51d49875b9f898
mtstall <mtstallings95@gmail.com>
2023-02-07 15:52:59 -0500

    updating html formatting generated by js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
-const html = require("./dist/generatehtml.js");
 
 // array of questions for user input
 const managerQuestions = [
@@ -170,30 +169,21 @@ function nextQuestions(response) {
 
 function generateHTML (teamArray) {
   console.log(teamArray);
-  // if (teamArray.getRole() === 'Manager') {
-  //   let xtrainfo = 
-  // }
-  console.log(teamArray[0].getRole());
-  console.log(teamArray[0].xtrainfo);
-  // for (let i = 0; i < teamArray.length; i++) {
-    //generate a card for each team member 
+    //generate html for a card for each team member within an array using .map
     const cardArray = teamArray.map( teamMember =>`
-    <div class="card">
-<div class="card-header">
-    <h2>${teamMember.empName}</h2>  
-    <h2><i class="far fa-user"></i> ${teamMember.getRole()}</h2>
-    <hr>
-</div>
-<div class="card-body">
-    <ul>
-        <li>ID: ${teamMember.id}</li>
-        <li>Email: ${teamMember.email}</li>
-        <li>${teamMember.xtrainfo} </li>
-    </ul>
-</div>
-</div>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${teamMember.empName}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${teamMember.getRole()}</h6>
+        <p class="card-text">
+            <ul>
+                <li>ID: ${teamMember.id}</li>
+                <li>Email: <a href="mailto:${teamMember.email}">${teamMember.email}</a></li>
+                <li>${teamMember.xtrainfo}</li>
+            </ul>
+        </p>
+    </div>
     `)
-  // }
 
   const htmlWrapper = `<!DOCTYPE html>
   <html lang="en">
@@ -201,13 +191,25 @@ function generateHTML (teamArray) {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="./style.css" />
+      <title>My Team</title>
   </head>
   <body>
+  <h1>My Team</h1>
+  <div id = "cards">
       ${cardArray.join("")}
+    </div>
   </body>
   </html>`;
-  console.log(htmlWrapper);
+  
+  // create index.html file with htmlWrapper variable
+  fs.writeFile(
+    "./dist/index.html",
+    `${htmlWrapper}
+    `,
+    (err) => (err ? console.log(err) : console.log("HTML generated!"))
+  );
 
 }
 
